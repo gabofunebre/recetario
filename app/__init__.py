1f04d88c3c02788160eb156674b40daeefa1e343
gabofunebre <136429132+gabofunebre@users.noreply.github.com>
2025-07-17 21:17:12 +0100

    Configurar Postgres en puerto 5433

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ def create_app():
     # Por defecto se conecta al contenedor "db" definido en docker-compose.
     database_url = os.getenv(
         'DATABASE_URL',
-        'postgresql://recetario:recetario@db:5432/recetario'
+        'postgresql://recetario:recetario@db:5433/recetario'
     )
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
